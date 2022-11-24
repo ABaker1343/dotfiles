98b40cc258afa259e08276023129abaae864b8c5
ABaker1343 <billybo1234@outlook.com>
2022-11-24 20:01:28 +0000

    updated sound change script

--- scripts/soundSelect.sh
+++ scripts/soundSelect.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
+pactl suspend-sink @DEFAULT_SINK@ true
 sinks=$(pactl list sinks | grep Name: | awk '{print $2}' | tr -d '<>')
 
 selection=$(echo "$sinks" | dmenu -i)
 
-pactl suspend-sink @DEFAULT_SINK@ 1
-
-pactl suspend-sink $selection 0
+pactl suspend-sink $selection false
 pactl set-default-sink $selection
