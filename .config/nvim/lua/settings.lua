3ffc35efbf9a4713fc6ff9fc966fa2243fd9c9d8
ABaker1343 <billybo1234@outlook.com>
2022-12-04 20:14:33 +0000

    updaeted keyboard configs

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -10,6 +10,7 @@ vim.opt.smartindent = true
 vim.opt.splitright = true
 
 vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
 
 vim.opt.guicursor="n-v-c:block,i:blinkwait500-blinkon250-blinkoff120-hor55"
 
