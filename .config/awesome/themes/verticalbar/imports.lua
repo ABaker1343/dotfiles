bd931247d25033ddb447f5a9bb64575502b6bde2
ABaker1343 <billybo1234@outlook.com>
2022-12-06 12:39:00 +0000

    changed compositor config

--- .config/awesome/themes/verticalbar/imports.lua
+++ .config/awesome/themes/verticalbar/imports.lua
@@ -85,9 +85,9 @@ function themeFuncs.startCompositor()
     local result = handle:read("*a")
     handle:close()
     if not result == "\n" then
-        awful.spawn.with_shell("killall picom && picom --config ~/.config/picom/picomdefault.conf")
+        awful.spawn.with_shell("killall picom && picom --config ~/.config/picom/picom.conf")
     else
-        awful.spawn.with_shell("picom --config ~/.config/picom/picomdefault.conf")
+        awful.spawn.with_shell("picom --config ~/.config/picom/picom.conf")
     end
 end
 
