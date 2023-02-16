c07bd25ed51528603953d9bd0829b3ac7525165b
ABaker1343 <billybo1234@outlook.com>
2023-02-16 09:31:20 +0000

    updated nvim config

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -12,7 +12,8 @@ vim.opt.splitright = true
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
 
-vim.opt.guicursor="n-v-c:block,i:blinkwait500-blinkon250-blinkoff120-hor55"
+--vim.opt.guicursor="n-v-c:block,i:blinkwait500-blinkon250-blinkoff120-hor55"
+vim.opt.guicursor="n-v-c:block,i:block"
 
 vim.api.nvim_set_keymap("n", "<C-t>", ":NvimTreeToggle<CR>", {})
 vim.api.nvim_set_keymap("n", "<C-f>", ":Telescope find_files<CR>", {})
