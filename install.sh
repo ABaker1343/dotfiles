cd0ef7af75e1c95e144d45e7a85136fe474266c9
ABaker1343 <billybo1234@outlook.com>
2023-05-02 22:08:49 +0000

    added tmux

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ cp -r .config/alacritty ~/.config/
 cp -r .config/nvim ~/.config/
 cp -r .config/ranger ~/.config/
 cp -r .config/zathura ~/.config/
+cp -r .config/tmux ~/.config/
 
 cp -r .config/i3 ~/.config/
 cp -r .config/i3status ~/.config/
--- update.sh
+++ update.sh
@@ -11,6 +11,7 @@ cp -r ~/.config/alacritty ~/.source/dotfiles/.config/
 cp -r ~/.config/nvim ~/.source/dotfiles/.config/
 cp -r ~/.config/ranger ~/.source/dotfiles/.config/
 cp -r ~/.config/zathura ~/.source/dotfiles/.config/
+cp -r ~/.config/tmux ~/.source/dotfiles/.config/
 
 cp -r ~/.config/i3 ~/.source/dotfiles/.config/
 cp -r ~/.config/i3status/ ~/.source/dotfiles/.config/
