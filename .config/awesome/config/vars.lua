5baa131e570765272af7047b7cd3f05a1aa3d5c2
ABaker1343 <billybo1234@outlook.com>
2023-06-03 18:32:39 +0100

    updates to bindings

--- .config/awesome/config/vars.lua
+++ .config/awesome/config/vars.lua
@@ -1,4 +1,5 @@
 modkey = "Mod4" -- super key
-terminal = "WINIT_X11_SCALE_FACTOR=1.0 alacritty"
+--terminal = "WINIT_X11_SCALE_FACTOR=1.0 alacritty"
+terminal = "kitty"
 editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
