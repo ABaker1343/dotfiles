727ce7356300b765963c76b9085c264557e1fa85
ABaker1343 <billybo1234@outlook.com>
2023-09-14 21:43:32 +0100

    updated scripts

--- config/sway/scripts/pretty.sh
+++ config/sway/scripts/pretty.sh
@@ -3,18 +3,18 @@ do
     case "$opt" in
         --pretty)
             swaymsg gaps outer all set 4;
-            #swaymsg gaps outer 4;
+            swaymsg gaps outer 4;
             swaymsg gaps inner all set 4;
-            #swaymsg gaps inner set 4;
+            swaymsg gaps inner 4;
             swaymsg smart_borders off;
             swaymsg default_border pixel 4
             swaymsg border pixel 4
             ;;
         --no-pretty)
             swaymsg gaps outer all set 0;
-            #swaymsg gaps outer 4;
+            swaymsg gaps outer 4;
             swaymsg gaps inner all set 0;
-            #swaymsg gaps inner 4;
+            swaymsg gaps inner 4;
             swaymsg smart_borders on;
             swaymsg default_border pixel 1
             swaymsg border pixel 1
