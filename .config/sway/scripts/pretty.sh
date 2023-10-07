219c26fe96c3bc86300a4c2f214229ae94dbfae0
ABaker1343 <billybo1234@outlook.com>
2023-10-07 14:46:44 +0100

    fixed sway config location

--- .config/sway/scripts/pretty.sh
+++ .config/sway/scripts/pretty.sh
@@ -12,9 +12,9 @@ do
             ;;
         --no-pretty)
             swaymsg gaps outer all set 0;
-            swaymsg gaps outer 4;
+            swaymsg gaps outer 0;
             swaymsg gaps inner all set 0;
-            swaymsg gaps inner 4;
+            swaymsg gaps inner 0;
             swaymsg smart_borders on;
             swaymsg default_border pixel 1
             swaymsg border pixel 1
